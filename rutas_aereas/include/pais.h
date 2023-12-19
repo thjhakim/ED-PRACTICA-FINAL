33e1fa037203fee350ab698764191f6df50fe347
thjhakim <146354520+thjhakim@users.noreply.github.com>
2023-12-19 13:06:34 +0100

    Update pais.h

--- rutas_aereas/include/pais.h
+++ rutas_aereas/include/pais.h
@@ -9,71 +9,102 @@
 #ifndef PAIS_H
 #define PAIS_H
 
-#include <iostream>
 #include "punto.h"
+#include <iostream>
+#include <cstring>
 using namespace std;
 
-class Pais{
-  private:
-    Punto punto;
-    string pais;
-    string bandera;
-    
-  public:
-    Pais(){}
-    
+/**
+ * @brief La clase Pais representa un país con información geográfica.
+ */
+class Pais {
+private:
+    Punto p; //Coordenadas geográficas del país.
+    string pais; // Nombre del país.
+    string bandera; //Bandera del país.
+
+public:
     /**
-     * @brief Obtiene el punto del pais.
-     * @return punto del pais.
+     * @brief Constructor por defecto para la clase Pais.
      */
-    Punto GetPunto()const{
-        return punto;
+    Pais() {}
+
+    /**
+     * @brief Obtener las coordenadas geográficas del país.
+     * @return Las coordenadas geográficas como un objeto Punto.
+     */
+    Punto GetPunto() const {
+        return p;
     }
-    
+
     /**
-     * @brief Obtiene el nombre del pais.
-     * @return Nombre del pais.
+     * @brief Obtener el nombre del país.
+     * @return Devuelve el nombre del país como un string.
      */
-    string GetPais()const{
+    string GetPais() const {
         return pais;
     }
-    
+
     /**
-     * @brief Obtiene la bandera del pais.
-     * @return Bandera del pais.
+     * @brief Obtener la bandera del país.
+     * @return Devuelve un string.
      */
-    string GetBandera()const{
+    string GetBandera() const {
         return bandera;
     }
-    
+
+    /**
+     * @brief Operador de comparación menor que sobrecargado para comparar países según sus coordenadas.
+     * @param P El país con el que se va a comparar.
+     * @return True si el país actual tiene coordenadas menores que el otro país.
+     */
     bool operator<(const Pais &P) const {
         return p < P.p;
     }
 
+    /**
+     * @brief Operador de igualdad sobrecargado para comparar países según coordenadas, nombre y bandera.
+     * @param P El país con el que se va a comparar.
+     * @return True si los países son iguales en coordenadas, nombre y bandera.
+     */
     bool operator==(const Pais &P) const {
         return (p == P.p) && (pais == P.pais) && (bandera == P.bandera);
     }
 
+    /**
+     * @brief Operador de igualdad sobrecargado para comparar con un objeto Punto según coordenadas.
+     * @param P El objeto Punto con el que se va a comparar.
+     * @return True si las coordenadas son iguales.
+     */
     bool operator==(const Punto &P) const {
         return p == P;
     }
-    
-    friend istream & operator>>(istream & is, Pais & P){
-        double lat,lng;
-	
-	is>>lat>>lng>>P.pais>>P.bandera;
-	
-	P.punto=Punto(lat,lng,"");
-	return is;
+
+    /**
+     * @brief Operador de flujo de entrada sobrecargado para leer información del país.
+     * @param is El flujo de entrada.
+     * @param P El objeto Pais en el que se va a leer.
+     * @return El flujo de entrada después de la lectura.
+     */
+    friend istream & operator>>(istream & is, Pais & P) {
+        double lat, lng;
+        is >> lat >> lng >> P.pais >> P.bandera;
+        P.p = Punto(lat, lng, "");
+        return is;
     }
-     
-    friend ostream & operator<<(ostream & os, const Pais &P){
-	os<<P.punto<<" "<<P.pais<<" "<<P.bandera<<endl;
-	return os;
+
+    /**
+     * @brief Operador de flujo de salida sobrecargado para imprimir información del país.
+     * @param os El flujo de salida.
+     * @param P El objeto Pais que se va a imprimir.
+     * @return El flujo de salida después de la impresión.
+     */
+    friend ostream & operator<<(ostream &os, const Pais &P) {
+        os << P.p << " " << P.pais << " " << P.bandera << endl;
+        return os;
     }
 };
 
+#endif // PAIS_H
 
-
-#endif /* PAIS_H */
 
