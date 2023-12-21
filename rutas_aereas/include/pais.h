4b55741b914c1d9fd27a1973e953773989478289
thjhakim <146354520+thjhakim@users.noreply.github.com>
2023-12-21 22:26:20 +0100

    Update pais.h
    dos metodos duplicados operator borrados

--- rutas_aereas/include/pais.h
+++ rutas_aereas/include/pais.h
@@ -3,7 +3,7 @@
  * File:   pais.h
  * Authors: Ángel Rodríguez Faya y Hakim Tahri
  * 
- * @brief Definición del  TDA Pais y sus métodos.
+ * @brief Definición del TDA Pais y sus métodos.
  */
 
 #ifndef PAIS_H
@@ -96,20 +96,6 @@ public:
     }
 
     /**
-     * @brief Operador de flujo de entrada sobrecargado para leer información del país.
-     * @param is El flujo de entrada.
-     * @param P El objeto Pais en el que se va a leer.
-     * @return El flujo de entrada después de la lectura.
-     */
-    friend istream & operator>>(istream & is, Pais & P) {
-        double lat, lng;
-        is >> lat >> lng >> P.pais >> P.bandera;
-        P.p = Punto(lat, lng, "");
-        return is;
-    }
-
-    
-    /**
      * @brief Operador de flujo de salida sobrecargado para imprimir información del país.
      * @param os El flujo de salida.
      * @param P El objeto Pais que se va a imprimir.
@@ -119,19 +105,6 @@ public:
 	os<<P.p<<" "<<P.pais<<" "<<P.bandera<<endl;
 	return os;
     }
-
-    /**
-     * @brief Operador de flujo de salida sobrecargado para imprimir información del país.
-     * @param os El flujo de salida.
-     * @param P El objeto Pais que se va a imprimir.
-     * @return El flujo de salida después de la impresión.
-     */
-    friend ostream & operator<<(ostream &os, const Pais &P) {
-        os << P.p << " " << P.pais << " " << P.bandera << endl;
-        return os;
-    }
 };
 
 #endif // PAIS_H
-
-
