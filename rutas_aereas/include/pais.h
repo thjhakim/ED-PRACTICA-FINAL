f5e1122b76926009992d1e80fdd3bbd3e5a1d418
thjhakim <146354520+thjhakim@users.noreply.github.com>
2023-12-22 15:09:23 +0100

    Update pais.h

--- rutas_aereas/include/pais.h
+++ rutas_aereas/include/pais.h
@@ -94,20 +94,6 @@ public:
 	P.p = Punto(lat,lng,"");
 	return is;
     }
-
-    /**
-     * @brief Operador de flujo de entrada sobrecargado para leer información del país.
-     * @param is El flujo de entrada.
-     * @param P El objeto Pais en el que se va a leer.
-     * @return El flujo de entrada después de la lectura.
-     */
-    friend istream & operator>>(istream & is, Pais & P) {
-        double lat, lng;
-        is >> lat >> lng >> P.pais >> P.bandera;
-        P.p = Punto(lat, lng, "");
-        return is;
-    }
-
     
     /**
      * @brief Operador de flujo de salida sobrecargado para imprimir información del país.
@@ -118,17 +104,6 @@ public:
     friend ostream & operator<<(ostream & os, const Pais &P){
 	os<<P.p<<" "<<P.pais<<" "<<P.bandera<<endl;
 	return os;
-    }
-
-    /**
-     * @brief Operador de flujo de salida sobrecargado para imprimir información del país.
-     * @param os El flujo de salida.
-     * @param P El objeto Pais que se va a imprimir.
-     * @return El flujo de salida después de la impresión.
-     */
-    friend ostream & operator<<(ostream &os, const Pais &P) {
-        os << P.p << " " << P.pais << " " << P.bandera << endl;
-        return os;
     }
 };
 
